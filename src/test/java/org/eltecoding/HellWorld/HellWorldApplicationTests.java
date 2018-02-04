ad6ba691e23387851ae822ad7f260c149cfc9a0f
LucasT <lukasz.tobola@gmail.com>
2018-02-04 16:03:15 +0100

    testing

---
+++ src/test/java/org/eltecoding/HellWorld/HellWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package org.eltecoding.HellWorld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
